17acb3d1801549234737a1c1d390dd8778a7eac2
Ahmed Khalaf <pxlshpr@gmail.com>
2022-10-26 16:31:43 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["EmojiPicker"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.196"),
+        .package(url: "https://github.com/pxlshpr/SwiftUISugar", from: "0.0.200"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
